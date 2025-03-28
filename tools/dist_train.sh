4023abe5831338fd0f90a8b7eb0cf6066521e40d
Chen Yang <2023225768@qq.com>
2025-03-28 05:41:16 +0000

    modify config and shell scripts

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -3,6 +3,4 @@ CONFIG=$1
 GPUS=$2
 PORT=${PORT:-29500}
 
-# OMP_NUM_THREADS=1 torchrun --nproc_per_node=$GPUS --master_port=$PORT $(dirname "$0")/train.py --dist $CONFIG ${@:3}   # $CONFIG <- $1，即传入的第一个参数传入到train.py中作为args.config; ${@:3}是将第3个参数到最后一个参数传入到train.py中比如--resume, --work_dir, --skip_validate等
-OMP_NUM_THREADS=1 torchrun --nproc_per_node=$GPUS --master_port=$PORT $(dirname "$0")/train.py $CONFIG ${@:3}   # $CONFIG <- $1，即传入的第一个参数传入到train.py中作为args.config; ${@:3}是将第3个参数到最后一个参数传入到train.py中比如--resume, --work_dir, --skip_validate等
-
+OMP_NUM_THREADS=1 torchrun --nproc_per_node=$GPUS --master_port=$PORT $(dirname "$0")/train.py --dist $CONFIG ${@:3}   # $CONFIG <- $1，即传入的第一个参数传入到train.py中作为args.config; ${@:3}是将第3个参数到最后一个参数传入到train.py中比如--resume, --work_dir, --skip_validate等
